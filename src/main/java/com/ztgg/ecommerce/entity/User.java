78fa4d1d701549b81b3590f0fccdd8fd54adf1f8
douyouzhe <douyouzhe@gmail.com>
2020-11-15 00:23:37 -0800

    add a test file

---
+++ src/main/java/com/ztgg/ecommerce/entity/User.java
@@ -0,0 +1,64 @@
+package com.ztgg.ecommerce.entity;
+
+import java.util.Date;
+
+public class User {
+	private Long userId;
+	private String name;
+	private String email;
+	private String profileImage;
+	private String gender;
+	//1. customer 2. merchant 3. admin
+	private Integer userType;
+	
+	private Date timeCreated;
+	private Date timeUpdated;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getProfileImage() {
+		return profileImage;
+	}
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+	public Date getTimeUpdated() {
+		return timeUpdated;
+	}
+	public void setTimeUpdated(Date timeUpdated) {
+		this.timeUpdated = timeUpdated;
+	}
+}
